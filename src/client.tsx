136f28f4931a7a57a6771f316581e75d91dd6150
cou <courange.c@gmail.com>
2024-04-10 15:05:55 +0900

    change prettier settings

--- src/client.tsx
+++ src/client.tsx
@@ -5,16 +5,16 @@ import { ClerkProvider } from '@clerk/clerk-react'
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
 
 export default function Header() {
-  return (
-    <header>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
-  )
+	return (
+		<header>
+			<SignedOut>
+				<SignInButton />
+			</SignedOut>
+			<SignedIn>
+				<UserButton />
+			</SignedIn>
+		</header>
+	)
 }
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -22,51 +22,51 @@ const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 if (!PUBLISHABLE_KEY) throw new Error('Missing Publishable Key')
 
 function App() {
-  return (
-    <>
-      <h1>Hello, Hono with React!</h1>
-      <h2>Example of useState()</h2>
-      <Counter />
-      <h2>Example of API fetch()</h2>
-      <ClockButton />
-    </>
-  )
+	return (
+		<>
+			<h1>Hello, Hono with React!</h1>
+			<h2>Example of useState()</h2>
+			<Counter />
+			<h2>Example of API fetch()</h2>
+			<ClockButton />
+		</>
+	)
 }
 
 function Counter() {
-  const [count, setCount] = useState(0)
-  return <button onClick={() => setCount(count + 1)}>You clicked me {count} times</button>
+	const [count, setCount] = useState(0)
+	return <button onClick={() => setCount(count + 1)}>You clicked me {count} times</button>
 }
 
 const ClockButton = () => {
-  const [response, setResponse] = useState<string | null>(null)
+	const [response, setResponse] = useState<string | null>(null)
 
-  const handleClick = async () => {
-    const response = await fetch('/api/clock')
-    const data = await response.json()
-    const headers = Array.from(response.headers.entries()).reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {})
-    const fullResponse = {
-      url: response.url,
-      status: response.status,
-      headers,
-      body: data,
-    }
-    setResponse(JSON.stringify(fullResponse, null, 2))
-  }
+	const handleClick = async () => {
+		const response = await fetch('/api/clock')
+		const data = await response.json()
+		const headers = Array.from(response.headers.entries()).reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {})
+		const fullResponse = {
+			url: response.url,
+			status: response.status,
+			headers,
+			body: data,
+		}
+		setResponse(JSON.stringify(fullResponse, null, 2))
+	}
 
-  return (
-    <div>
-      <button onClick={handleClick}>Get Server Time</button>
-      {response && <pre>{response}</pre>}
-    </div>
-  )
+	return (
+		<div>
+			<button onClick={handleClick}>Get Server Time</button>
+			{response && <pre>{response}</pre>}
+		</div>
+	)
 }
 
 const domNode = document.getElementById('root')!
 const root = createRoot(domNode)
 root.render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-    <Header />
-    <App />
-  </ClerkProvider>
+	<ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+		<Header />
+		<App />
+	</ClerkProvider>
 )
